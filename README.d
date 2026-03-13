16c9bc98d9ff6fdeda9139a9a9e79b653e332cfc
michassm <michelm240703@gmail.com>
2026-03-12 22:14:12 -0300

    Update README with project information
    Add project description and details to README

--- README.d
+++ README.d
@@ -1,1 +1,9 @@
 # ContadorRegressivo
+
+## Exiba uma contagem de 10 até 0.
+
+Projeto proposto para estudar e aplicar conhecimentos adquiridos aos estudos de Java 21.
+
+IDE: IntelliJ IDE
+
+Java version: jdk 21
