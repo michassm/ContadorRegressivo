2543724c18fdc4b84b01ff0820b55ff49b0abc87
michassm <michelm240703@gmail.com>
2026-03-12 22:11:41 -0300

    Contador Regressivo

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0 ; i--){
+            System.out.println("Contagem regresiva: "+i);
+            if (i == 0){
+                System.out.println("Acabou!");
+            }
+        }
+    }
+}
